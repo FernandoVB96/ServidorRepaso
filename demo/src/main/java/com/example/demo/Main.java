b9b829a05100a43271e1717c940847da7cb3dc0d
Fernando <fernando.vaquero@a.vedrunasevillasj.es>
2024-09-18 17:40:46 +0200

    subiendo proyecto

--- demo/src/main/java/com/example/demo/Main.java
+++ demo/src/main/java/com/example/demo/Main.java
@@ -1,7 +1,31 @@
 package com.example.demo;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String rutaArchivo = "C:\\Users\\fvaquero\\2 DAW\\SERVIDORES";
+        // Crear un objeto File con la ruta
+        File archivo = new File(rutaArchivo);
+        // Verificar si el archivo existe
+        if (archivo.exists()) {
+            // Usar BufferedReader para leer el archivo línea por línea
+            try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+                String linea;
+                
+                // Leer cada línea del archivo y mostrarla por consola
+                while ((linea = br.readLine()) != null) {
+                    System.out.println(linea);
+                }
+                
+            } catch (IOException e) {
+                System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
+            }
+        } else {
+            System.out.println("El archivo no existe en la ruta especificada.");
+        }
+
     }
 }
